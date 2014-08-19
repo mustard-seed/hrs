606ec03d4157e93e9bfb44133658f429266b6e55
jeppler <jayson@Alisons-MacBook-Air.local>
2014-08-18 19:57:21 -0700

    most issues fixed
    just ave to get the FSPL issue worked out

--- jeppler_hsrsar_backproj.m
+++ jeppler_hsrsar_backproj.m
@@ -17,40 +17,47 @@ fd_samp = 1000.0; %baseband sample rate (Hz)
 t_range = [0, 10.0]; %data acquisition time interval
 
 %define geometry
-p_t = [0, 0, 30]; %transmitter location
+%p_t = [0, 0, 30]; %transmitter location
+p_t = [0, 0, 0]; %transmitter location...SIMPLIFIED FOR DEBUGGING
 v = [0.0, 100.0, 0.0]; %constant velocity of receiver on train
-p_r_0 = [50, -500, 4.5]; %initial position of receiver on train
-p_s_1 = [0, 0, 0.0]; %scatterer 1
+%p_r_0 = [50, -500, 4.5]; %initial position of receiver on train
+p_r_0 = [0, -500, 0]; %initial position of receiver on train ...SIMPLIFIED FOR DEBUGGING
+p_s_1 = [20, 0, 0.0]; %scatterer 1
+x = p_s_1(1); y = p_s_1(2); z = p_s_1(3);%test one point
 
 %derived values
 t = t_range(1): 1/fd_samp:t_range(2);
 fc_vec = fc-fc_band/2:del_fc:fc+fc_band/2;
-fc_band = fc_vec(end) - fc_vec(1);
+fc_vec = ifftshift(fc_vec);
+%fc_band = fc_vec(end) - fc_vec(1);
+fc_band = max(fc_vec)-min(fc_vec);
+
 img = zeros(length(x), length(y), length(z));
 img = complex(img, img);
 alpha=0.0; %random phase offset
-
 %simulate data acquisition
 signal = get_signal(p_t, p_s_1,fc_vec,alpha,v, p_r_0, t); %baseband signal per tone
 
 %range compression
-signal_rc = fft(signal,[],1); %range comressed signal
-signal_rc_ex = [signal_rc' (signal_rc(1,:))']'; %concatenate zero range row to the end to enable wrapped interpolation
+%signal_rc = fft(signal,[],1); %range comressed signal
+signal_rc = flipud(ifft(signal,[],1)); %range compressed signal ... flip needed due to sign convention of signal phase
+JDE_TODO correct the path loss here (minor issue however)
+imagesc(abs(signal_rc));figure(gcf);
 
-%JDE_TODO correct range compressed data for free space path loss
-FSPL = 20 * log10(rTotal)+ 20 * log10(fc(i_fc))-147.55;
-
+signal_rc_ex = [signal_rc' (signal_rc(1,:))']'; %concatenate zero range
+%row to the end to enable wrapped interpolation in the case when the range lies between last and first value
 
 %set-up interpolation of range compressed data
 [n_rng, n_az] = size(signal_rc);
 del_rng = c/fc_band; %m
+
 ranges = (0:n_rng-1)*del_rng;
 [X, Y] = ndgrid([ranges, ranges(end)+del_rng], t);
 GI = griddedInterpolant(X,Y, signal_rc_ex); %set-up interpolation object
 
 %back-projection over image grid
 range_interval = n_rng*del_rng;
-lambda = c/fc_vec(1);
+lambda = c/fc;
 del_y_factor = (doppler_fraction/sqrt(1-doppler_fraction^2));
 for ii = 1:length(x)
     ii
@@ -63,6 +70,9 @@ for ii = 1:length(x)
             t_segment = t_range_segment(1): 1/fd_samp:t_range_segment(2);
             rTotal_ = r_total(p_t, p_s,v, p_r_0, t_segment);
             rTotal = mod(rTotal_, range_interval);
+            test1 =  exp(complex(0,2*pi*(rTotal)./lambda));
+            test2 = GI(rTotal, t_segment)/fspl(rTotal, fc);
+            
             img(ii,jj,kk) = sum(GI(rTotal, t_segment).*exp(complex(0,-2*pi*(rTotal)./lambda)));
         end
     end
@@ -120,11 +130,11 @@ function rTotal = r_total(p_t_, p_s_,v, p_r_0, t)
     %total range
     rTotal = Rts_norm + Rsr_norm;
 end
-%--------------------------------------------------------------------------
 
-function FSPL = FSPL(range, f)
-    FSPL_log = 20 * log10(range)+ 20 * log10(f)-147.55; 
-    FSPL = 10.^(-FSPL_log/20);
+%--------------------------------------------------------------------------
+function fspl_linear = fspl(range, fc)
+    fspl_log = 20 * log10(range)+ 20 * log10(fc)-147.55; 
+    fspl_linear = 10.^(-fspl_log/20);
 end
 
 %--------------------------------------------------------------------------
@@ -134,16 +144,12 @@ rTotal = r_total(p_t, p_s, v, p_r_0, t);
 signal = zeros(length(fc), length(rTotal));
 for i_fc = 1:length(fc)
     lambda = c/fc(i_fc);
-    
     % Free space path loss
     %FSPL = 20 * log10(rTotal)+ 20 * log10(fc(i_fc))-147.55; 
     %FSPL_linear = 10.^(-FSPL/20);
-    
-    %JDE_TODO undo this!!!
-    FSPL = FSPL(rTotal,fc(i_fc));
-    %FSPL = ones(1, length(rTotal));
-    
+    FSPL_linear = fspl(rTotal, fc(i_fc));
     % Receive Power
-    signal(i_fc, :) = FSPL.*exp(complex(0,alpha + 2*pi*(rTotal)./lambda));
+    signal(i_fc, :) = FSPL_linear.*exp(complex(0,alpha + 2*pi*(rTotal)./lambda));
+    %signal(i_fc, :) = exp(complex(0,alpha + 2*pi*(rTotal)./lambda));
 end
 end
